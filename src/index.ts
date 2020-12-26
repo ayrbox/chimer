1c7838a2f6e9a7a91425d95d7369bd983c2a75c4
ayrbox <sabin.dangol@hotmail.com>
2020-12-26 16:56:25 +0000

    Add express endpooint to convert html to pdf.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-const msg: string = 'Hello TS world';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-console.log(msg);
+import { htmlToPdf } from './htmlToPdf';
+
+const app = express();
+
+app.post(
+  '/pdf/:fileName',
+  bodyParser.text({ type: 'text/html' }),
+  async (req, res) => {
+    const { body: htmlToConvert, params } = req;
+
+    const fileName = `${params.fileName || 'unknown'}.pdf`;
+
+    try {
+      const pdfBuffer = await htmlToPdf(htmlToConvert);
+      res.attachment(fileName);
+      res.contentType('application/pdf');
+      res.send(pdfBuffer);
+    } catch (err) {
+      throw err;
+    }
+  }
+);
+
+app.listen(3000, () => {
+  console.log('Listenning at 3000');
+});
